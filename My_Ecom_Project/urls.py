9c1b9bc78fb60d7bbeba1150f9ac12474797139b
Dipankershaha <dipankershaha@gmail.com>
2021-04-24 23:00:59 +0600

    resolve settings

--- My_Ecom_Project/urls.py
+++ My_Ecom_Project/urls.py
@@ -1,6 +1,8 @@
 
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls import url
+from django.views.static import serve
 
 from django.conf import settings
 from django.contrib.staticfiles.urls import static, staticfiles_urlpatterns
@@ -12,6 +14,9 @@ urlpatterns = [
    path('account/', include('App_Login.urls')),
    path('shop/', include('App_Order.urls')),
    path('payment/', include('App_Payment.urls')),
+
+   url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
+    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
 ]
 
 urlpatterns += staticfiles_urlpatterns()
