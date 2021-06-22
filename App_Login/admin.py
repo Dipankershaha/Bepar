99ce873cbbca3d8815a99273ef7bddf21c702e1b
wahidSpiro <wahid@spiro.study>
2021-06-22 13:07:43 +0600

    security issue added

--- App_Login/admin.py
+++ App_Login/admin.py
@@ -4,5 +4,22 @@ from App_Login.models import User, Profile
 # Register your models here.
 
 
-admin.site.register(User)
+
+
+class CustomProfile(admin.ModelAdmin):
+    model = User
+
+    list_display = ['email','is_staff']
+    actions = ['make_stuff', 'make_user', 'block', 'unBlock']
+
+    def make_stuff(self,request, queryset):
+        queryset.update(is_staff = True)
+    def make_user(self,request, queryset):
+        queryset.update(is_staff = False)
+    def block(self,request, queryset):
+        queryset.update(is_active = False)
+    def unBlock(self,request, queryset):
+        queryset.update(is_active = True)
+
+admin.site.register(User, CustomProfile)
 admin.site.register(Profile)
--- My_Ecom_Project/settings.py
+++ My_Ecom_Project/settings.py
@@ -15,6 +15,7 @@ import os
 import cloudinary
 import cloudinary.uploader
 import cloudinary.api
+from decouple import config
 
 import django_heroku
 import dj_database_url
@@ -22,7 +23,7 @@ import dj_database_url
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 # BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-TEMPLATES_DIR = os.path.join(BASE_DIR,'templates')
+TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 STATIC_DIR = os.path.join(BASE_DIR, 'static')
 MEDIA_DIR = os.path.join(BASE_DIR, 'media')
 
@@ -31,12 +32,12 @@ MEDIA_DIR = os.path.join(BASE_DIR, 'media')
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '23=n(2$(1w*#uix6=_zs*2v*&%h-o7gk$pf_5w%0@h8^f43+r)'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG')
 
-ALLOWED_HOSTS = ['*','bepar.herokuapp.com']
+ALLOWED_HOSTS = ['*', 'bepar.herokuapp.com']
 
 
 # Application definition
@@ -57,7 +58,7 @@ INSTALLED_APPS = [
 ]
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
-#custom user model
+# custom user model
 AUTH_USER_MODEL = 'App_Login.User'
 
 MIDDLEWARE = [
@@ -76,7 +77,7 @@ ROOT_URLCONF = 'My_Ecom_Project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [TEMPLATES_DIR,],
+        'DIRS': [TEMPLATES_DIR, ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -147,16 +148,16 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
-#MEDIA
+# MEDIA
 MEDIA_ROOT = MEDIA_DIR
 MEDIA_URL = '/media/'
 
 LOGIN_URL = '/account/login/'
 
-# cloudinary.config( 
-#   cloud_name = "dc3hi9hib", 
-#   api_key = "256577772793599", 
-#   api_secret = "t5OuqhfoIYahEUYi3KJeffL7faA" 
+# cloudinary.config(
+#     cloud_name=config('cloud_name'),
+#     api_key=config('api_key'),
+#     api_secret=config('api_secret'),
 # )
 
 django_heroku.settings(locals())
