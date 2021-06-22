611922aa00490154d399f7d9465a8c789334bbd6
wahidSpiro <wahid@spiro.study>
2021-06-22 13:22:59 +0600

    security issue added

--- App_Payment/views.py
+++ App_Payment/views.py
@@ -15,8 +15,11 @@ from sslcommerz_python.payment import SSLCSession
 from decimal import Decimal
 import socket
 from django.views.decorators.csrf import csrf_exempt
+from decouple import config
 
 # Create your views here.
+
+
 @login_required
 def checkout(request):
     saved_address = BillingAddress.objects.get_or_create(user=request.user)
@@ -30,11 +33,12 @@ def checkout(request):
             form = BillingForm(instance=saved_address)
             messages.success(request, f"Shipping Address Saved!")
     order_qs = Order.objects.filter(user=request.user, ordered=False)
-    #print(order_qs)
+    # print(order_qs)
     order_items = order_qs[0].orderitems.all()
-    #print(order_items)
+    # print(order_items)
     order_total = order_qs[0].get_totals()
-    return render(request, 'App_Payment/checkout.html', context={"form":form, "order_items":order_items, "order_total":order_total, "saved_address":saved_address})
+    return render(request, 'App_Payment/checkout.html', context={"form": form, "order_items": order_items, "order_total": order_total, "saved_address": saved_address})
+
 
 @login_required
 def payment(request):
@@ -48,26 +52,30 @@ def payment(request):
         messages.info(request, f"Please complete profile details!")
         return redirect("App_Login:profile")
 
-    store_id = 'none5e026730bdf7f'
-    API_key = 'none5e026730bdf7f@ssl'
-    mypayment = SSLCSession(sslc_is_sandbox=True, sslc_store_id=store_id, sslc_store_pass=API_key)
+    store_id = config('store_id')
+    API_key = config('API_key')
+    mypayment = SSLCSession(sslc_is_sandbox=True,
+                            sslc_store_id=store_id, sslc_store_pass=API_key)
 
     status_url = request.build_absolute_uri(reverse("App_Payment:complete"))
-    #print(status_url)
-    mypayment.set_urls(success_url=status_url, fail_url=status_url, cancel_url=status_url, ipn_url=status_url)
+    # print(status_url)
+    mypayment.set_urls(success_url=status_url, fail_url=status_url,
+                       cancel_url=status_url, ipn_url=status_url)
 
     order_qs = Order.objects.filter(user=request.user, ordered=False)
     order_items = order_qs[0].orderitems.all()
     order_items_count = order_qs[0].orderitems.count()
     order_total = order_qs[0].get_totals()
 
-    mypayment.set_product_integration(total_amount=Decimal(order_total), currency='BDT', product_category='Mixed', product_name=order_items, num_of_item=order_items_count, shipping_method='Courier', product_profile='None')
-
+    mypayment.set_product_integration(total_amount=Decimal(order_total), currency='BDT', product_category='Mixed',
+                                      product_name=order_items, num_of_item=order_items_count, shipping_method='Courier', product_profile='None')
 
     current_user = request.user
-    mypayment.set_customer_info(name=current_user.profile.full_name, email=current_user.email, address1=current_user.profile.address_1, address2=current_user.profile.address_1, city=current_user.profile.city, postcode=current_user.profile.zipcode, country=current_user.profile.country, phone=current_user.profile.phone)
+    mypayment.set_customer_info(name=current_user.profile.full_name, email=current_user.email, address1=current_user.profile.address_1, address2=current_user.profile.address_1,
+                                city=current_user.profile.city, postcode=current_user.profile.zipcode, country=current_user.profile.country, phone=current_user.profile.phone)
 
-    mypayment.set_shipping_info(shipping_to=current_user.profile.full_name, address=saved_address.address, city=saved_address.city, postcode=saved_address.zipcode, country=saved_address.country)
+    mypayment.set_shipping_info(shipping_to=current_user.profile.full_name, address=saved_address.address,
+                                city=saved_address.city, postcode=saved_address.zipcode, country=saved_address.country)
 
     response_data = mypayment.init_payment()
     return redirect(response_data['GatewayPageURL'])
@@ -82,12 +90,15 @@ def complete(request):
         if status == 'VALID':
             val_id = payment_data['val_id']
             tran_id = payment_data['tran_id']
-            messages.success(request,f"Your Payment Completed Successfully! Page will be redirected!")
-            return HttpResponseRedirect(reverse("App_Payment:purchase", kwargs={'val_id':val_id, 'tran_id':tran_id},))
+            messages.success(
+                request, f"Your Payment Completed Successfully! Page will be redirected!")
+            return HttpResponseRedirect(reverse("App_Payment:purchase", kwargs={'val_id': val_id, 'tran_id': tran_id},))
         elif status == 'FAILED':
-            messages.warning(request, f"Your Payment Failed! Please Try Again! Page will be redirected!")
+            messages.warning(
+                request, f"Your Payment Failed! Please Try Again! Page will be redirected!")
 
     return render(request, "App_Payment/complete.html", context={})
+
 
 @login_required
 def purchase(request, val_id, tran_id):
@@ -103,6 +114,7 @@ def purchase(request, val_id, tran_id):
         item.purchased = True
         item.save()
     return HttpResponseRedirect(reverse("App_Shop:home"))
+
 
 @login_required
 def order_view(request):
